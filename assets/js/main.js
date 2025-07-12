f9fbcad73ebbcdee91761800843a5cdf472c0e44
abaldwin06 <abaldwin06@users.noreply.github.com>
2025-07-12 14:46:16 -0600

    training page copy

--- assets/js/main.js
+++ assets/js/main.js
@@ -238,7 +238,7 @@ document.addEventListener('DOMContentLoaded', () => {
     AOS.init({
       duration: 1000,
       easing: 'ease-in-out',
-      once: true,
+      once: false,
       mirror: false
     });
   }
